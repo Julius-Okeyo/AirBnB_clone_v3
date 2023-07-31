b6eb1bfe83223186f95e9f61b9a69c095f799d41
Julius-Okeyo <jaykopiyo@gmail.com>
2023-07-31 09:46:23 -0700

    Added views

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
 """
-Initialize an api endpoint
+Flask App that integrates with AirBnB static HTML Template
 """
-from flask import Flask
-from models import storage
 from api.v1.views import app_views
+from flask import Flask, jsonify, make_response, render_template, url_for
+from flask_cors import CORS, cross_origin
+from flasgger import Swagger
+from models import storage
 import os
+from werkzeug.exceptions import HTTPException
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
+swagger = Swagger(app)
 app.url_map.strict_slashes = False
-
 host = os.getenv('HBNB_API_HOST', '0.0.0.0')
 port = os.getenv('HBNB_API_PORT', 5000)
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+app.register_blueprint(app_views)
 
 @app.teardown_appcontext
 def teardown_db(exception):
@@ -22,8 +26,44 @@ def teardown_db(exception):
     """
     storage.close()
 
+@app.errorhandler(400)
+def handle_404(exception):
+    """
+    handle Error 400
+    """
+    code = exception.__str__().split()[0]
+    description = exception.description
+    message = {'error': description}
+    return make_response(jsonify(message), code)
+
+
+@app.errorhandler(Exception)
+def global_error_handler(err):
+    """
+    handle all errors
+    """
+    if isinstance(err, HTTPException):
+        if type(err).__name__ == 'NotFound':
+            err.description = "Not found"
+        message = {'error': err.description}
+        code = err.code
+    else:
+        message = {'error': err}
+        code = 500
+    return make_response(jsonify(message), code)
+
+
+def setup_global_errors():
+    """
+    update HTTPException Class with custom error function
+    """
+    for cls in HTTPException.__subclasses__():
+        app.register_error_handler(cls, global_error_handler)
+
+
 if __name__ == "__main__":
     """
-    run App
+    run flask app
     """
+    setup_global_errors()
     app.run(host=host, port=port)
